1095162fb1efd8a991c48644e2f92b3b7ef7cb75
delimaalves.gustavo <delimaalves.gustavo@gmail.com>
2020-10-29 20:01:24 -0300

    Initial

---
+++ ExcecoesPersonalizadas/Program.cs
@@ -0,0 +1,50 @@
+﻿using ExcecoesPersonalizadas.Entities;
+using ExcecoesPersonalizadas.Entities.Exceptions;
+using System;
+
+namespace ExcecoesPersonalizadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine("Room number: ");
+                int number = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Check-in (dd/MM/yyyy: ");
+                DateTime checkin = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("Check-out (dd/MM/yyyy: ");
+                DateTime checkout = DateTime.Parse(Console.ReadLine());
+
+                Reservation reservation = new Reservation(number, checkin, checkout);
+                Console.WriteLine("Reservation: " + reservation);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation: ");
+
+                Console.WriteLine("Check-in (dd/MM/yyyy: ");
+                checkin = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("Check-out (dd/MM/yyyy: ");
+                checkout = DateTime.Parse(Console.ReadLine());
+
+                reservation.UpdateDates(checkin, checkout);
+                Console.WriteLine("Reservation: " + reservation);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Error in reservation: " + e.Message);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Format Error: " + e.Message);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+        }
+    }
+}
